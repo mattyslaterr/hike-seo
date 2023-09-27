32f472c4c4434f6d04c0df5c6425e8db15430d0e
mathewslater <nggmatty@gmail.com>
2023-09-27 12:14:41 +0100

    - Updated users migration to include first name, last name and phone number - Added users role migration - Added bookings table migration - Added blocked bookings table migration - Updated environment file for database credentials per sail

--- database/migrations/2023_09_27_105339_create_bookings_table.php
+++ database/migrations/2023_09_27_105339_create_bookings_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bookings', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('phone_number')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id');
+            $table->dateTime('time');
+            $table->string('vrm');
+            $table->string('make');
+            $table->string('model');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bookings');
     }
 };
